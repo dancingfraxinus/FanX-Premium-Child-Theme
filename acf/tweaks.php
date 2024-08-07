6973a17c60101f9f63be090b69a794caaa13852f
Liz Moore <ashelizmoore@gmail.com>
2024-08-07 17:26:01 -0600

    Merge branch 'main' of https://github.com/dancingfraxinus/FanX-Premium-Child-Theme

--- acf/tweaks.php
+++
@@ -1,2 +0,0 @@
-<?php  //For Tweaks to ACF Fields 
-
--- shortcode.php
+++ shortcode.php
@@ -3,14 +3,7 @@
 //Shortcodes in ACF 
 add_filter('acf/format_value/type=textarea', 'do_shortcode');
 add_filter('acf/format_value/type=text', 'do_shortcode');
-add_filter('acf/format_value/type=message', 'do_shortcode');
-add_filter( 'wp_title', 'do_shortcode');
-add_filter( 'the_title', 'do_shortcode');
-add_filter( 'single_post_title', 'do_shortcode');
 add_filter( 'wp_nav_menu', 'do_shortcode');
-add_filter( 'widget_text', 'do_shortcode');
-add_filter( 'the_excerpt', 'do_shortcode');
-
 
 // -- [page_title] -->
 
@@ -45,13 +38,6 @@ function thembreak_df() {
 	return '<hr style="width:50%; text-align:left; ; margin: 3%; border-top: 1px solid gold;">';
 }
 add_shortcode( 'hr', 'thembreak_df' );
-
-//--- [year] Used in Socket -->
-function year_shortcode () {
-   $year = date_i18n ('Y');
-   return $year;
-   }
-   add_shortcode ('year', 'year_shortcode');
 
 
 //-- Shortcode for ACF Output --->>>
